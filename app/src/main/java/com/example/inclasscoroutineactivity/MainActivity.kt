2ea6891816554ab1093f1b32af21b87bd5659db6
jzcodess <tuq67092@temple.edu>
2023-03-29 00:10:33 -0400

    3/28/23

--- app/src/main/java/com/example/inclasscoroutineactivity/MainActivity.kt
+++ app/src/main/java/com/example/inclasscoroutineactivity/MainActivity.kt
@@ -2,15 +2,34 @@ package com.example.inclasscoroutineactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var countDown: TextView
+    lateinit var startButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        CoroutineScope(Job() + Dispatchers.Default).launch {
-            delay(1000)
+        countDown = findViewById(R.id.countDown)
+        startButton = findViewById(R.id.startButton)
+
+        startButton.setOnClickListener{
+            startCountdown()
         }
     }
+
+        private fun startCountdown() {
+            CoroutineScope(Job() + Dispatchers.Main).launch {
+                for (i in 100 downTo 0) {
+                    countDown.text = i.toString()
+                    delay(1000)
+                }
+            }
+        }
 }
